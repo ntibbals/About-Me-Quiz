0454ab7229371f2ce1a595520157bbab443608ba
ntibbals <ntibbals@gmail.com>
2019-01-07 19:49:16 -0800

    rough completion of q1

--- Lab01-About-Me-Quiz/Lab01-About-Me-Quiz/Program.cs
+++ Lab01-About-Me-Quiz/Lab01-About-Me-Quiz/Program.cs
@@ -6,7 +6,30 @@ namespace Lab01_About_Me_Quiz
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            bool runGame = true;
+            while (runGame)
+            {
+                runGame = true;
+                Console.WriteLine("Welcome to a quiz all about me, Nate.");
+                int answer1 = QuestionOne();
+                if (answer1 == 2)
+                {
+                    Console.WriteLine("Correct! I have 2 cats.");
+                }
+                else
+                    Console.WriteLine("Incorrect. I actually have 2 cats.");
+                Console.ReadLine();
+            }
+        }
+
+        static int QuestionOne()
+        {
+            Console.WriteLine("How many cats do I have?");
+            string input = Console.ReadLine();
+
+            int guess = Convert.ToInt32(input);
+
+            return guess;
         }
     }
 }
